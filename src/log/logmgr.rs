87a4e1ab09613ebd8a30d6bf72c61f0d4a28179b
ytshuhar-wq <ytshuhar@gmail.com>
2026-06-13 17:59:41 +0300

    Update logmgr.rs

--- src/log/logmgr.rs
+++ src/log/logmgr.rs
@@ -1,9 +1,10 @@
 use super::logger;
+use dirs_next::data_local_dir;
 pub fn log_manager(message: &str) {
-    let mut path = dirs_next::data_local_dir()
+    let mut path = data_local_dir()
         .unwrap_or_else(|| std::path::PathBuf::from("."));
 
-    path = path.join("R-touch").join("logs").join("r-touch.log");
+    path = path.join("R-touch").join("logs").join("r-touch.log"); // home_dir/R-touch/logs/r-touch.log
 
     if let Err(e) = logger::Logger::log(path.to_str().unwrap(), &message) {
         eprintln!("Error logging the action. Error: {e}");
